41c3e450c98150461e340e76e50d557a2fd7a377
Kundan Kumar Keshri <101567147+kundan799@users.noreply.github.com>
2022-05-19 22:46:56 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import css from './Navbar.css'
+const Navbar=()=>{
+    return  (
+        <>
+        
+        <div className="nav">
+            <div className="app">
+                <h1>Logobackery</h1>
+            </div>
+            <div className="app">
+                <p>Service</p>
+                <p>project</p>
+                <p>About</p>
+            </div>
+            <div className="app">
+                <button>Contect</button>
+            </div>
+
+        </div>
+        </>
+    );
+    
+   
+   
+
+
+    
+}
+export default Navbar;
---
+++ src/components/avi.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+ 
+let Index=()=>{
+    return (
+        <>
+        <h3>Mobile Operating System</h3>
+        <ul>
+            <li>Android</li>
+            <li>Black barry</li>
+            <li>iphone</li>
+            <li>windows phone </li>
+        </ul>
+        <h3>Mobile Manufacturing</h3>
+        <ul>
+            <li>Samsung</li>
+            <li>Apple</li>
+            <li>Htc</li>
+            <li>Micromax</li>
+        </ul>
+        </>
+
+    );
+}
+export default Index;
